cc9aae2fcfd3c1391ac9386d62505ec89f7e591f
Premkumar Nagaiah <138997281+PremN-IT@users.noreply.github.com>
2026-04-08 02:38:42 +0530

    Add AWS provider and VPC resource configuration

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+region = "var.region"
+}
+resource "aws_vpc" "main" {
+  cidr_block       = "var.cidr"
+  instance_tenancy = "default"
+  enable_dns_hostnames = true
+  tags = {
+    Name = "main"
+  }
+}
